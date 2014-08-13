bef4844bb830ba280dd68b0afd5318384c1361c4
Richard Huynh <richardbaohuynh@gmail.com>
2014-08-13 16:57:01 -0400

    Starts adding interactivity for entering items

--- js/app.js
+++ js/app.js
@@ -1,2 +1,48 @@
 $(document).ready(function() {
+	$('.main').fadeIn(1000, function() {
+	});
+
+	$('#item-box').keypress(function(event) {
+		if (event.which == 13) {
+			addItem($(this).val());
+		}
+	});
+
+	$('#store-button').click(function() {
+		var itemName = $('#item-box').val();
+		if (itemName) {
+			addItem(itemName);
+		}
+	});
+
 });
+
+
+// take in name of item
+function addItem(item) {
+	//alert('attempted to add ' + name);
+
+	// remove text from textbox
+	$('#item-box').val('');
+
+	// make a new table row at the top for new item
+	//$('table > tr:first').before('<tr><td></td><td>item</td><td></td></tr>');
+	//$('<tr><td></td><td>item</td><td></td></tr>').prependTo('table');
+
+	//insert after
+	$('<tr><td></td><td>item</td><td></td></tr>').insertAfter($('#enter-item-row'));
+
+	// add pics and everything
+}
+
+function checkItem() {
+
+}
+
+function uncheckItem() {
+
+}
+
+function removeItem() {
+
+}
